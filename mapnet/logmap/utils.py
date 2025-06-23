5c759415562e51f4c15b246d63b7117b0d63c41e
buz galbraith <buzgalbraith@gmail.com>
2025-06-23 16:37:02 -0400

    added a check for logmap to prevent re-running

--- mapnet/logmap/utils.py
+++ mapnet/logmap/utils.py
@@ -48,80 +48,83 @@ def run_logmap(
     **_,
 ):
     output_path = output_path or os.path.join(os.getcwd(), "mapnet", "logmap", "output")
-    os.makedirs(output_path, exist_ok=True)
-    if target_onto_file == None:
-        if target_def == None:
-            raise ValueError("must define either target_onto_file or target_def")
-        else:
-            target_name = (
-                target_def["prefix"] + ".obo"
-                if not target_def["subset"]
-                else os.path.join(
-                    target_def["subset_name"], target_def["prefix"] + ".obo"
-                )
-            )
-            target_onto_file = os.path.join(
-                target_def["prefix"], target_def["version"], target_name
-            )
-    if source_onto_file == None:
-        if source_def == None:
-            raise ValueError("must define either source_onto_file or source_def")
-        else:
-            source_name = (
-                source_def["prefix"] + ".obo"
-                if not source_def["subset"]
-                else os.path.join(
-                    source_def["subset_name"], source_def["prefix"] + ".obo"
-                )
-            )
-            source_onto_file = os.path.join(
-                source_def["prefix"], source_def["version"], source_name
-            )
-    # ## define the java command to run
-    java_cmd = [
-        "java",
-        "-jar",
-        "-Xmx32g",
-        "--add-opens",
-        "java.base/java.lang=ALL-UNNAMED",
-        "/package/logmap-matcher-4.0.jar",
-        "MATCHER",
-        f"file:///package/resources/{shlex.quote(target_onto_file)}",
-        f"file:///package/resources/{shlex.quote(source_onto_file)}",
-        "/package/output/",
-        "true",  # classify input ontology as well as map
-    ]
-    ##  define the command to run
-    if singularity:
-        cmd = [
-            "apptainer",
-            "run",
-            "-B",
-            f"{shlex.quote(output_path)}:/package/output",  # Mount where output will be written
-            "-B",
-            f"{shlex.quote(dataset_dir)}:/package/resources/",  ## mount directory with resources  
-            "mapnet/logmap/container/logmap.sif",
-            "sh",
-            "-c",
-            shlex.join(java_cmd),  ## add the java command as a string
-        ]
+    if os.path.exists(os.path.join(output_path, 'logmap2_mappings.tsv')):
+        logging.info(f"{os.path.join(output_path, 'logmap2_mappings.tsv')} already exists skipping!")
     else:
-        cmd = [
-            "docker",
-            "run",
-            "--rm",
-            "-v",
-            f"{shlex.quote(output_path)}:/package/output",  # Mount where output will be written
-            "-v",
-            f"{shlex.quote(dataset_dir)}:/package/resources/",  ## mount directory with resources
-            f"logmap:{shlex.quote(tag)}",  ## specify the image and tag
-            "sh",
-            "-c",
-            shlex.join(java_cmd),  ## add the java command as a string
+        os.makedirs(output_path, exist_ok=True)
+        if target_onto_file == None:
+            if target_def == None:
+                raise ValueError("must define either target_onto_file or target_def")
+            else:
+                target_name = (
+                    target_def["prefix"] + ".obo"
+                    if not target_def["subset"]
+                    else os.path.join(
+                        target_def["subset_name"], target_def["prefix"] + ".obo"
+                    )
+                )
+                target_onto_file = os.path.join(
+                    target_def["prefix"], target_def["version"], target_name
+                )
+        if source_onto_file == None:
+            if source_def == None:
+                raise ValueError("must define either source_onto_file or source_def")
+            else:
+                source_name = (
+                    source_def["prefix"] + ".obo"
+                    if not source_def["subset"]
+                    else os.path.join(
+                        source_def["subset_name"], source_def["prefix"] + ".obo"
+                    )
+                )
+                source_onto_file = os.path.join(
+                    source_def["prefix"], source_def["version"], source_name
+                )
+        # ## define the java command to run
+        java_cmd = [
+            "java",
+            "-jar",
+            "-Xmx32g",
+            "--add-opens",
+            "java.base/java.lang=ALL-UNNAMED",
+            "/package/logmap-matcher-4.0.jar",
+            "MATCHER",
+            f"file:///package/resources/{shlex.quote(target_onto_file)}",
+            f"file:///package/resources/{shlex.quote(source_onto_file)}",
+            "/package/output/",
+            "true",  # classify input ontology as well as map
         ]
-    logger.info(cmd)
-    # run the command
-    subprocess.check_call(cmd)
+        ##  define the command to run
+        if singularity:
+            cmd = [
+                "apptainer",
+                "run",
+                "-B",
+                f"{shlex.quote(output_path)}:/package/output",  # Mount where output will be written
+                "-B",
+                f"{shlex.quote(dataset_dir)}:/package/resources/",  ## mount directory with resources  
+                "mapnet/logmap/container/logmap.sif",
+                "sh",
+                "-c",
+                shlex.join(java_cmd),  ## add the java command as a string
+            ]
+        else:
+            cmd = [
+                "docker",
+                "run",
+                "--rm",
+                "-v",
+                f"{shlex.quote(output_path)}:/package/output",  # Mount where output will be written
+                "-v",
+                f"{shlex.quote(dataset_dir)}:/package/resources/",  ## mount directory with resources
+                f"logmap:{shlex.quote(tag)}",  ## specify the image and tag
+                "sh",
+                "-c",
+                shlex.join(java_cmd),  ## add the java command as a string
+            ]
+        logger.info(cmd)
+        # run the command
+        subprocess.check_call(cmd)
 
 
 def logmap_arg_factory(
